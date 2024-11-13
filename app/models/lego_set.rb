34fcc79d4e90b927a89fbf0d015428526f94a419
JustinR <justinreimer0@gmail.com>
2024-11-13 01:53:21 -0600

    Attempted to add modifications to activeadmin resources and broke them

--- app/models/lego_set.rb
+++ app/models/lego_set.rb
@@ -1,5 +1,9 @@
 class LegoSet < ApplicationRecord
   has_many :reviews
   has_many :order_items
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["id", "lego_set_id", "name", "review", "rating", "created_at", "updated_at"]
+  end
 end
 
