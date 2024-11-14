ce4818a5ed99ee2e926308f588845ba0ba6f84d8
JustinR <justinreimer0@gmail.com>
2024-11-13 19:46:57 -0600

    Added search functionality to the nav bar by modifying the controller in legosets

--- app/controllers/lego_sets_controller.rb
+++ app/controllers/lego_sets_controller.rb
@@ -1,6 +1,21 @@
 class LegoSetsController < ApplicationController
   def index
-    @lego_sets = LegoSet.page(params[:page]).per(10)
+    # Search Lego sets by title
+    @lego_sets = if params[:query].present?
+                   LegoSet.where("title ILIKE ?", "%#{params[:query]}%")
+                 else
+                   LegoSet.all
+                 end
+
+    # Sort Lego sets by price (high to low or low to high)
+    if params[:sort] == "price_high_to_low"
+      @lego_sets = @lego_sets.order(price: :desc)
+    elsif params[:sort] == "price_low_to_high"
+      @lego_sets = @lego_sets.order(price: :asc)
+    end
+
+    # Paginate results
+    @lego_sets = @lego_sets.page(params[:page]).per(10)
   end
   def show
     @lego_set = LegoSet.find(params[:id])
