a87a70f2e44113aeadcf0a50e3ba286fe59dab81
JustinR <justinreimer0@gmail.com>
2024-11-25 19:27:00 -0600

    Added cart session for guest users in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :current_cart
+
+  def current_cart
+    session[:cart] ||= {}
+  end
+
+  def add_to_cart(lego_set_id)
+    cart = current_cart
+    cart[lego_set_id.to_s] ||= 0
+    cart[lego_set_id.to_s] += 1
+    session[:cart] = cart
+  end
+
+  def remove_from_cart(lego_set_id)
+    cart = current_cart
+    cart.delete(lego_set_id.to_s)
+    session[:cart] = cart
+  end
 end
