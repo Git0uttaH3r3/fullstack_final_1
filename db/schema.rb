377e00145e60eea233e6cc5a24510ef1c3a3fd58
JustinR <justinreimer0@gmail.com>
2024-11-29 02:48:24 -0600

    Added a category model to later create categories in active admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_29_023437) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_29_084544) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -40,6 +40,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_29_023437) do
   create_table "admins", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
